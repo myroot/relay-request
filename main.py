1887446007982429070d3754a6ad90bdaeae35c4
Seungkeun Lee <hi.root@gmail.com>
2013-01-02 15:56:50 +0900

    replace urllib to urlfetch

--- main.py
+++ main.py
@@ -17,27 +17,7 @@
 import webapp2
 import urllib
 import urllib2
-
-def http_post( url ,args):
-    args = urllib.urlencode(args)
-    request = urllib2.Request( url, args )
-    request.add_header('Referer', url)
-    request.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:14.0) Gecko/20100101 Firefox/14.0.1')
-    response = urllib2.urlopen(request)
-    return response
-
-def http_get( url ,args):
-    args = urllib.urlencode(args)
-    if url.find('?') == -1 :
-        url = '%s?%s'%(url,args)
-    else:
-        url = '%s&%s'%(url,args)
-
-    request = urllib2.Request( url )
-    request.add_header('Referer', url)
-    request.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:14.0) Gecko/20100101 Firefox/14.0.1')
-    response = urllib2.urlopen(request)
-    return response
+from google.appengine.api import urlfetch
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -45,44 +25,38 @@ class MainHandler(webapp2.RequestHandler):
 
 class proxyHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write("test")
-
-class RequestHandler(webapp2.RequestHandler):
-    def get(self):
-        reqtype = self.request.get('reqtype')
-        url = self.request.get('requrl')
+        url = self.request.get('rq')
         args = dict(self.request.params)
-
-        if args.has_key('requrl'):
-            args.pop('requrl')
-        if args.has_key('reqtype'):
-            args.pop('reqtype')
-
-        response = http_get( url, args)
-        if reqtype == 'xml' :
-            self.response.headers["Content-Type"] = "text/xml"
+        if args.has_key('rq'):
+            args.pop('rq')
+        args = urllib.urlencode(args)
+        if url.find('?') == -1 :
+            url = '%s?%s'%(url,args)
         else:
-            self.response.headers["Content-Type"] = "text/html"
-        self.response.write(response.read())
+            url = '%s&%s'%(url,args)
+
+        response = urlfetch.fetch(url=url, 
+                                  method=urlfetch.GET,
+                                  headers=self.request.headers)
+
+        self.response.headers = response.headers
+        self.response.write(response.content)
 
     def post(self):
-        reqtype = self.request.get('reqtype')
-        url = self.request.get('requrl')
+        url = self.request.get('rq')
         args = dict(self.request.params)
+        if args.has_key('rq'):
+            args.pop('rq')
+        args = urllib.urlencode(args)
+        response = urlfetch.fetch(url=url, 
+                                  payload=args, 
+                                  method=urlfetch.POST,
+                                  headers=self.request.headers)
 
-        if args.has_key('requrl'):
-            args.pop('requrl')
-        if args.has_key('reqtype'):
-            args.pop('reqtype')
-        response = http_post( url, args)
-        if reqtype == 'xml' :
-            self.response.headers["Content-Type"] = "text/xml"
-        else:
-            self.response.headers["Content-Type"] = "text/html"
-        self.response.write(response.read())
+        self.response.headers = response.headers
+        self.response.write(response.content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/request', RequestHandler),
     ('/r', proxyHandler)
 ], debug=True)
